cfe29fdcba4d3ea625fd96463b1018ff793e11d5
Stiven Vallejo <stivjo96@gmail.com>
2026-07-01 21:32:10 -0500

    [UPDATE] alerta stock bajo

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def p_pro(op, x, p, c, t):
             t1 = datos1[3]
             pf1 = float(datos1[4])
             print(f"{x1} | ${p1} | {c1} unidades | {t1} | ${pf1}")
+            if c1 < 5:print("⚠ ALERTA: Stock bajo")
         print("--------------------------------------------------")
 
     elif op == 3:
@@ -72,7 +73,7 @@ def p_pro(op, x, p, c, t):
 if __name__ == "__main__":
     print("--- SISTEMA DE INVENTARIO VIEJO V1.0 ---")
     # Registrar un par de productos de prueba
-    p_pro(1, "Laptop", 800.0, 5, "Tecnología")
+    p_pro(1, "Laptop", 800.0, 3, "Tecnología")
     p_pro(1, "Cuaderno", 2.50, 50, "Útiles")
     
     # Listar productos
